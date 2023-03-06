6103727bbc5d50c1be1e941c39fc7d794b09d2e3
Hyden Polikoff <hyden.dev@gmail.com>
2023-03-06 10:32:05 -0700

    turn RecipeModel into environment object

---
+++ RecipeListApp/Views/RecipeFeatureView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeFeatureView.swift
+//  RecipeListApp
+//
+//  Created by Hyden Polikoff on 3/6/23.
+//
+
+import SwiftUI
+
+struct RecipeFeatureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeatureView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeatureView()
+    }
+}
